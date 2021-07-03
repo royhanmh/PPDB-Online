b3d4dc346402653c4399b1724881c1987c1519bd
royhanmh <84570532+royhanmh@users.noreply.github.com>
2021-07-03 20:42:43 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://ppdb-uas.herokuapp.com/';
+$config['base_url'] = 'https://daring-ppdb.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
